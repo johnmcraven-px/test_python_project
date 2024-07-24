945012104c48cc51ca005c22a69037690b600d5b
John McRaven <john.mcraven@physicsx.ai>
2024-07-24 17:05:39 -0400

    fix dict

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ with open('../data/input1.json') as f:
 with open('../data/input2.json') as f:
     input2 = json.load(f)
 
-z = input1.x + input2.y
+z = input1["x"] + input2["y"]
 
 # Write the result to output.json
 with open('../data/output.json', 'w') as f:
