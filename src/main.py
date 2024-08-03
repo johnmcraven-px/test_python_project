58b1b0ee9903d25ac2b6e6f3eafe6e598f2b5680
John McRaven <john.mcraven@physicsx.ai>
2024-08-02 21:04:01 -0400

    updates

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ app = FastAPI()
 lastVegaSpecs: Dict[str, str] | None = None
 
 def generate_initial_chart(df: pd.DataFrame, initial_selection):
-    selection = alt.selection_single(fields=['Batch', 'Run'], nearest=True, on='click', empty='none', init=initial_selection)
+    selection = alt.selection_single(fields=['Batch', 'Run'], nearest=True, on='click', empty='none', value=[initial_selection])
     scatter_plot = alt.Chart(df).mark_circle(size=100).encode(
         x='Performance:Q',
         y='ConstraintSatisfaction:Q',
