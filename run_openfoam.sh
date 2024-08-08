f0738ec44ca6ca7be3dad6d64ed2259941bcb131
John McRaven <john.mcraven@physicsx.ai>
2024-08-08 16:56:15 -0400

    updates

--- run_openfoam.sh
+++ run_openfoam.sh
@@ -1,6 +1,7 @@
 cp -r ../data/output output
 apt-get update && apt-get install -y python3-pip
 su - openfoam
+whoami
 pip install pandas
 cd /git
 echo "source /opt/openfoam10/etc/bashrc" >> ~/.bashrc
