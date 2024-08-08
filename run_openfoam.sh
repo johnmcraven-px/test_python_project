9acf40df95b665d9b7504ef195523e920152c5ce
John McRaven <john.mcraven@physicsx.ai>
2024-08-08 16:53:46 -0400

    updates

--- run_openfoam.sh
+++ run_openfoam.sh
@@ -1,11 +1,11 @@
 cp -r ../data/output output
 apt-get update && apt-get install -y python3-pip
-pip install pandas
 su - openfoam
+pip install pandas
 cd /git
 echo "source /opt/openfoam10/etc/bashrc" >> ~/.bashrc
 mkdir case
 mkdir ../data/case
 
-python openfoam_test.py "$@"
+python3 openfoam_test.py "$@"
 mv case/* ../data/case/
