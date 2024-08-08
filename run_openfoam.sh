545b39e0b4f98a269d91814fb5988920fb448dfd
John McRaven <john.mcraven@physicsx.ai>
2024-08-08 01:49:19 -0400

    updates

--- run_openfoam.sh
+++ run_openfoam.sh
@@ -4,6 +4,7 @@ rm -rf output
 cp -r ../data/output output
 docker build -t my_openfoam_image .
 apt-get update && apt-get install -y tmux && apt-get install -y docker.io
+systemctl start docker
 cd case
 tmux new-session -d -s container_session "bash -c 'docker run -it --name openfoam_container_new -v $(pwd):/home/openfoam/case my_openfoam_image'"
 cd ..
