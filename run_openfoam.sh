84f6337b88de7176072cb7df2387c89b2abce17a
John McRaven <john.mcraven@physicsx.ai>
2024-08-08 02:03:00 -0400

    updates

--- run_openfoam.sh
+++ run_openfoam.sh
@@ -4,7 +4,11 @@ rm -rf output
 cp -r ../data/output output
 docker build -t my_openfoam_image .
 apt-get update && apt-get install -y tmux && apt-get install -y docker.io
-systemctl start docker
+echo "Docker status 1"
+sudo systemctl status docker
+sudo systemctl start docker
+echo "Docker status 2"
+sudo systemctl status docker
 cd case
 tmux new-session -d -s container_session "bash -c 'docker run -it --name openfoam_container_new -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/home/openfoam/case my_openfoam_image'"
 cd ..
