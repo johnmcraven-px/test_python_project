96a3e5f0a593fb42832a97b01bc40c2989f160a9
John McRaven <john.mcraven@physicsx.ai>
2024-08-08 01:44:17 -0400

    updates

--- run_openfoam.sh
+++ run_openfoam.sh
@@ -6,6 +6,8 @@ docker build -t my_openfoam_image .
 apt-get update && apt-get install -y tmux
 cd case
 tmux new-session -d -s container_session "bash -c 'docker run -it --name openfoam_container_new -v $(pwd):/home/openfoam/case my_openfoam_image'"
+cd ..
 
 pip install pandas
 python openfoam_test.py
+docker rm -f openfoam_container_new
