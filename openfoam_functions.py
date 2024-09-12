1e52084daa302991b64cac39918e460d2d2720f8
MarkDHuntington23 <mark.huntington@physicsx.ai>
2024-09-12 19:39:40 +0100

    adding back some better mesh settings

--- openfoam_functions.py
+++ openfoam_functions.py
@@ -716,13 +716,13 @@ castellatedMeshControls
 
 snapControls
 {
-    nSmoothPatch 3;
-    tolerance 4.0;
-    nSolveIter 300;
-    nRelaxIter 5;
+    nSmoothPatch 10;
+    tolerance 2.0;
+    nSolveIter 500;
+    nRelaxIter 10;
     nFeatureSnapIter 10;
     implicitFeatureSnap true;
-    explicitFeatureSnap false;
+    explicitFeatureSnap true;
     multiRegionFeatureSnap true;
 }
 
@@ -753,7 +753,7 @@ addLayersControls
 meshQualityControls
 {
     maxNonOrtho 65;
-    maxBoundarySkewness 20;
+    maxBoundarySkewness 4;
     maxInternalSkewness 4;
     maxConcave 80;
     minVol 1e-13;
@@ -764,11 +764,11 @@ meshQualityControls
     minFaceWeight 0.05;
     minVolRatio 0.01;
     minTriangleTwist -1;
-    nSmoothScale 4;
+    nSmoothScale 5;
     errorReduction 0.75;
     relaxed
     {
-        maxNonOrtho 75;
+        maxNonOrtho 80;
     }
 }
 
